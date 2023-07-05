40950ccd7c4fafe326a45d995e11eb667251ff29
UrielBelo <urielhbelo@gmail.com>
2023-07-05 12:25:50 -0300

    Publicaçã do Site

---
+++ main.js
@@ -0,0 +1,19 @@
+const $imagesGrid = document.getElementById('imagesGrid')
+const $$imgOnGridF = document.getElementsByClassName('imgOnGridF')
+
+for(let i=1; i<=8; i++){
+    const el = document.createElement('img')
+    el.setAttribute('src',`./assets/${i}.jpg`)
+    el.setAttribute('alt',`${i}`)
+    el.classList.add('imgOnGrid')
+    el.addEventListener('mousedown', (ev) => {
+        const elClicked = ev.target.alt
+        window.location.href = `./assets/${elClicked}.jpg`
+    })
+    $imagesGrid.appendChild(el)
+}
+
+$$imgOnGridF[0].addEventListener('mousedown', (ev) => {
+    const elClicked = ev.target.alt
+    window.location.href = `./assets/${elClicked}.jpg`
+})
