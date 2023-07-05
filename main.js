4f8fb5acaf36db316d0b063432f0012801741919
UrielBelo <urielhbelo@gmail.com>
2023-07-05 12:32:16 -0300

    Adição do Clique no Vídeo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const $imagesGrid = document.getElementById('imagesGrid')
 const $$imgOnGridF = document.getElementsByClassName('imgOnGridF')
+const $video = document.getElementById('video')
 
 for(let i=1; i<=8; i++){
     const el = document.createElement('img')
@@ -16,4 +17,8 @@ for(let i=1; i<=8; i++){
 $$imgOnGridF[0].addEventListener('mousedown', (ev) => {
     const elClicked = ev.target.alt
     window.location.href = `./assets/${elClicked}.jpg`
+})
+
+$video.addEventListener('mousedown', () => {
+    window.location.href = `./assets/Video-PRONTO.mp4`
 })
